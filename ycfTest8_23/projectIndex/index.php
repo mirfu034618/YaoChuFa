f1124965ba225699316382a358eba6edb7f99173
mirfu034618 <18789153695@163.com>
2016-08-24 10:22:14 +0800

    企业网站首页重布局

---
+++ ycfTest8_23/projectIndex/index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>金陵贸易有限公司</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<!-----------------顶端----------------------------->
+<div id="boxOne">
+    <div id="boxOneLeft"><h1 title="金陵贸易有限公司"><img src="images/logo.gif"></h1></div>
+	<div id="boxOneRight"><a href="#"><img src="images/btn_ibg.gif">设为主页</a>&nbsp;<a href="#"><img src="images/btn_fbg.gif">加入收藏</a></div>
+</div>
+<!----------------导航栏----------------------------->
+<div id="boxTwo">
+    <div id="boxTwoLeft">
+		<table>
+		   <tr>
+			   <td><a href="#">首  页</a></td>
+			   <td><img src="images/nav_line.gif"></td>
+			   <td><a href="#">公司简介</a></td>
+			   <td><img src="images/nav_line.gif"></td>
+			   <td><a href="#">产品展示</a></td>
+			   <td><img src="images/nav_line.gif"></td>
+			   <td><a href="#">行业资讯</a></td>
+			   <td><img src="images/nav_line.gif"></td>
+			   <td><a href="#">客户留言</a></td>
+			   <td><img src="images/nav_line.gif"></td>
+			   <td><a href="#">联系我们</a></td>
+		   </tr>
+		</table>
+	</div>
+	<div id="boxTwoRight">
+		2016-8-23 16:21:45
+	</div>
+</div>
+<!----------------------------------------------------->
+<div id="boxFree"><a href="#"><img src="images/banner.jpg"></a></div>
+<!--------------------内容--------------------------->
+<div id="boxFour">
+    <div id="boxFourTop">
+		<!---------------公司简介---------------->
+		<div id="boxFourTopLeft">
+			<div id="aboutUs">
+			   <div class="aboutUsPic"><span class="aboutUs">公司简介</span><span class="aboutUb">About Us</span></div>
+			</div>
+			<div id="content">
+				<div id="contentTitle">
+				  我公司前身是金能保温材料经营部，因扩大经营规模，于2009年3月变更为金陵贸易有限公司。1994年开始从事保温、保冷、吸音、耐火、化工、建材等产品的经营贸易，对控制产品质量方面具备非常好的经验，是中国东南地区最大的保温材料经营贸易公司之一。公司经营宗旨是：诚信经营，质量第一。欢迎您的洽谈！
+				  我公司前身是金能保温材料经营部，因扩大经营规模，于2009年3月变更为金陵贸易有限公司。
+				</div>
+				<div id="contentMore"><a href="#">查看更多...</a></div>
+			</div>
+		</div>
+		<!-----------最新公告列表-------------->
+		<div id="boxFourTopRight">
+            <div id="news">
+				 <div class="aboutUsPic"><span class="aboutUs">最新公告</span><span class="aboutUb">news</span></div>
+			</div>
+			<div id="newsList">
+				 <ul>
+					<li>
+						<a title="新闻1新闻1新闻1" href="#">新闻1新闻1新闻1新闻1</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+					<li>
+						<a title="新闻2新闻2新闻2" href="#">新闻2新闻2新闻2新闻2</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+					<li>
+						<a title="新闻1新闻1新闻1" href="#">新闻1新闻1新闻1新闻1</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+					<li>
+						<a title="新闻1新闻1新闻1" href="#">新闻1新闻1新闻1新闻1</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+				 </ul>
+			</div>
+		</div>
+	</div>
+	<!----------------------------------->
+	<div id="boxFourCenter">
+		<!-------------产品展示--------------->
+		<div id="boxFourCenterLeft">
+            <div id="product">
+			  <div class="aboutUsPic">
+				<span class="aboutUs">产品展示</span><span class="aboutUb">products</span>
+			  </div>
+			  <span class="more"><a href="#">更多...</a></span>
+		    </div>
+			<div id="products">
+				<ul>
+					<li>
+                        <span>
+                          <a href="#"><img src="images/prod1.gif" alt="222222" /></a>
+                        </span>
+						<span class="productTitle"><a href="#">产品1</a></span>
+					</li>
+					<li>
+                        <span>
+                          <a href="#"><img src="images/prod2.gif" alt="222222" /></a>
+                        </span>
+						<span class="productTitle"><a href="#">产品2</a></span>
+					</li>
+					<li>
+                        <span>
+						  <a href="#"><img src="images/prod2.gif" alt="222222" /></a>
+                        </span>
+						<span class="productTitle"><a href="#">产品3</a></span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<!-------------资讯列表--------------->
+		<div id="boxFourCenterRight">
+            <div id="information">
+				<div class="aboutUsPic"><span class="aboutUs">行业资讯</span><span class="aboutUb">information</span></div>
+				<span class="infotMore"><a href="#">更多...</a></span>
+			</div>
+			<div id="informationList">
+				<ul>
+					<li>
+						<a title="新闻1新闻1新闻1" href="#">新闻1新闻1新闻1新闻1</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+					<li>
+						<a title="新闻2新闻2新闻2" href="#">新闻2新闻2新闻2新闻2</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+					<li>
+						<a title="新闻1新闻1新闻1" href="#">新闻1新闻1新闻1新闻1</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+					<li>
+						<a title="新闻1新闻1新闻1" href="#">新闻1新闻1新闻1新闻1</a>
+						<span class="dateTime">2016-08-10 13:20:12</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!------------------链接盒子-------------------->
+	<div id="boxFive">
+		<div id="link">
+			<div class="aboutUsPic"><span class="aboutUs">友情链接</span><span class="aboutUb">links</span></div>
+		</div>
+		<div id="links">
+			<table>
+				<tr>
+					<td><a href="http://www.baidu.com">百度</a></td>
+					<td>|</td>
+					<td><a href="http://www.yaochufa.com">要出发</a></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<!------------------------------------------>
+	<div id="boxSix"></div>
+	<!------------------------------------------>
+	<div id="boxSeven">
+		<p>地址：广东省广州市广州大道北  联系人：乐可   移动电话：13619829982 固定电话：020-1234567 传 真：020-1234567</p>
+		<p>Copyright @ 2009 金陵贸易有限公司 版权所有</p>
+		<p><a href="#">粤ICP备08108790号</a></p>
+	</div>
+</div>
+</body>
+</html>
